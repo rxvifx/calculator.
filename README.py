6dca5ca3d6b7470ee42ab568406fcd8e3c610671
Ravi Singh <99658075+rxvifx@users.noreply.github.com>
2022-03-08 15:20:15 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,17 @@
+# calculator
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print("only type number of the operation")
+print('''select operation for calculation-
+1.additon 2.division 3.subtraction 4.multiplicati>
+c=input()
+if c == "1":
+  print("bro its:",a+b)
+elif c == "2":
+  print("bro its:",a/b)
+elif c == "3":
+  print("bro its:",a-b)
+elif c == "4":
+  print("bro its:",a*b)
+elif c == "5":
+  print("bro its:",a*a)
